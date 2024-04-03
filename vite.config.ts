bb639375b3f8a092fd2dbc060b3687ec33877f4f
mktbsh <t93.hasebe@gmail.com>
2024-04-03 19:33:20 +0900

    [fix] ローカル開発時のbaseパス設定を明示的にundefinedに

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const slug = process.env.REPO_SLUG;
-if (!slug) {
-  throw new Error('REPO_SLUG is not set');
-}
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: `/${slug}/`,
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const slug = process.env.REPO_SLUG;
+  if (command === "build" && !slug) {
+    throw new Error('REPO_SLUG is not set');
+  }
+
+  return {
+    base: slug ? `/${slug}/` : undefined,
+    plugins: [react()],
+  }
 })
