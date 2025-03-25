75ba81e2479d5a9603f3b2766567dad111d1438a
mktbsh <t93.hasebe@gmail.com>
2025-03-26 00:52:30 +0900

    biome: fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+async function main() {
+	const selector = "#root";
+	const target = document.querySelector(selector);
+	if (!target) throw new Error(`Could not found: ${selector}`);
+
+	return ReactDOM.createRoot(target).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
+}
+
+main();
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const slug = process.env.REPO_SLUG;
-  if (command === "build" && !slug) {
-    throw new Error('REPO_SLUG is not set');
-  }
+	const slug = process.env.REPO_SLUG;
+	if (command === "build" && !slug) {
+		throw new Error("REPO_SLUG is not set");
+	}
 
-  return {
-    base: slug ? `/${slug}/` : undefined,
-    plugins: [react()],
-  }
-})
+	return {
+		base: slug ? `/${slug}/` : undefined,
+		plugins: [react()],
+	};
+});
