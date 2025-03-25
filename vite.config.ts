8c634ac847026b73972f7b3507aec04feb656fde
mktbsh <t93.hasebe@gmail.com>
2025-03-26 00:53:32 +0900

    feat(vite.config.ts): add Tailwind CSS plugin to Vite configuration for improved styling capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -10,6 +11,6 @@ export default defineConfig(({ command }) => {
 
 	return {
 		base: slug ? `/${slug}/` : undefined,
-		plugins: [react()],
+		plugins: [react(), tailwindcss(),],
 	};
 });
